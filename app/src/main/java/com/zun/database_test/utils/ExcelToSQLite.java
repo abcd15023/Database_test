c43010fbdf9da2f5e6f19e3c01fe6c78311d0b57
abcd15023 <abcd15023@qq.com>
2022-08-21 17:17:21 +0800

    对Excel读取方法进行注释

--- app/src/main/java/com/zun/database_test/utils/ExcelToSQLite.java
+++ app/src/main/java/com/zun/database_test/utils/ExcelToSQLite.java
@@ -95,7 +95,11 @@ public class ExcelToSQLite {
             @Override
             public void run() {
                 try {
+                    //读取excel数据，只做了这件事
                     working(new FileInputStream(file));
+
+
+
                     if (listener != null) {
                         handler.post(new Runnable() {
                             @Override
@@ -123,67 +127,98 @@ public class ExcelToSQLite {
     }
 
     private void working(InputStream stream) throws Exception {
+        //一般excel库分为， workbook【整个表格】,sheet【单个表格选项】,cell【单元格】
+        //拿到workbook
         HSSFWorkbook workbook = new HSSFWorkbook(stream);
         int sheetNumber = workbook.getNumberOfSheets();
+
+        //遍历表格数量，建成不同的数据库表
         for (int i = 0; i < sheetNumber; i++) {
             createTable(workbook.getSheetAt(i));
         }
+
+        //关闭数据库
         database.close();
     }
 
     private void createTable(Sheet sheet) {
         Cursor cursor = null;
         try {
+            String tableName = sheet.getSheetName();
+            //构建建表数据库SQL语句
             StringBuilder createTableSql = new StringBuilder("CREATE TABLE IF NOT EXISTS ");
-            createTableSql.append(sheet.getSheetName());
+            createTableSql.append(tableName);//表名
             createTableSql.append("(");
-            Iterator<Row> rit = sheet.rowIterator();
-            Row rowHeader = rit.next();
-            List<String> columns = new ArrayList<>();
-            for (int i = 0; i < rowHeader.getPhysicalNumberOfCells(); i++) {
-                createTableSql.append(rowHeader.getCell(i).getStringCellValue());
-                if (i == rowHeader.getPhysicalNumberOfCells() - 1) {
-                    createTableSql.append(" TEXT");
+
+            //拿到sheet的迭代器，row的迭代器
+            Iterator<Row> rowIterator = sheet.rowIterator();
+            //这里只next了一次，所以取的是第一行，就我们的标题头
+            Row titleRow = rowIterator.next();
+
+            //建立一个 标题头的List容器
+            List<String> titleNameList = new ArrayList<>();
+            for (int index = 0; index < titleRow.getPhysicalNumberOfCells(); index++) {
+
+                String titleName = titleRow.getCell(index).getStringCellValue();
+                //添加到List容器
+                titleNameList.add(titleName);
+
+                //添加到SQL
+                createTableSql.append(titleName);
+                if (index == titleRow.getPhysicalNumberOfCells() - 1) {
+                    createTableSql.append(" TEXT");//如果是行末，就不需要逗号
                 } else {
                     createTableSql.append(" TEXT,");
                 }
-                columns.add(rowHeader.getCell(i).getStringCellValue());
             }
             createTableSql.append(")");
 
-            if (dropTable)
-                database.execSQL("DROP TABLE IF EXISTS " + sheet.getSheetName());
-
+            if (dropTable){
+                //如果需要删除旧表，则执行
+                database.execSQL("DROP TABLE IF EXISTS " + tableName);
+            }
+            //执行SQL
             database.execSQL(createTableSql.toString());
 
-            for (String column : columns) {
-                cursor = database.rawQuery("SELECT * FROM " + sheet.getSheetName(), null);
-                // grab cursor for all data
-                int deleteStateColumnIndex = cursor.getColumnIndex(column);  // see if the column is there
-                if (deleteStateColumnIndex < 0) {
+            //新增数据库字段
+            for (String titleName : titleNameList) {
+                // 根据 sheetName，获取到titleName的Index，如果Index小于0，则表示未找到
+                cursor = database.rawQuery("SELECT * FROM " + tableName, null);
+                int titleNameIndex = cursor.getColumnIndex(titleName);
+
+                if (titleNameIndex < 0) {
+                    //未找到，则添加
                     String type = "TEXT";
-                    // missing_column not there - add it
-                    database.execSQL("ALTER TABLE " + sheet.getSheetName()
-                            + " ADD COLUMN " + column + " " + type + " NULL;");
+                    database.execSQL("ALTER TABLE " + tableName
+                            + " ADD COLUMN " + titleName + " " + type + " NULL;");
                 }
             }
 
-            while (rit.hasNext()) {
-                Row row = rit.next();
-                ContentValues values = new ContentValues();
-                for (int n = 0; n < row.getPhysicalNumberOfCells(); n++) {
-                    if (row.getCell(n).getCellTypeEnum() == CellType.NUMERIC) {
-                        values.put(columns.get(n), row.getCell(n).getNumericCellValue());
+            //通过迭代器，生成对应的ContentValues，然后执行插入语句
+            while (rowIterator.hasNext()) {
+                //第一次next()，得到的结果在上面，是title行；此处再次next()得到的是数据行
+                Row dataRow = rowIterator.next();
+
+                ContentValues valuesToInsert = new ContentValues();
+                for (int index = 0; index < dataRow.getPhysicalNumberOfCells(); index++) {
+
+                    if (dataRow.getCell(index).getCellTypeEnum() == CellType.NUMERIC) {
+                        //使用put(String key, Double value)将cell的数据存入valuesToInsert
+                        valuesToInsert.put(titleNameList.get(index), dataRow.getCell(index).getNumericCellValue());
+
                     } else {
-                        values.put(columns.get(n), row.getCell(n).getStringCellValue());
+                        valuesToInsert.put(titleNameList.get(index), dataRow.getCell(index).getStringCellValue());
                     }
                 }
-                long result = database.insertWithOnConflict(sheet.getSheetName(),
-                        null, values, SQLiteDatabase.CONFLICT_IGNORE);
+                //执行数据库插入操作
+                long result = database.insertWithOnConflict(tableName, null, valuesToInsert, SQLiteDatabase.CONFLICT_IGNORE);
+
+                //插入结果的判断
                 if (result < 0) {
                     throw new RuntimeException("Insert value failed!");
                 }
             }
+
         } finally {
             if (cursor != null)
                 cursor.close();
